af846b07ac0a69023171a980271ff5b00fce83b4
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2020-02-23 22:12:59 +0100

    Game logic in Table component

--- src/Table.js
+++ src/Table.js
@@ -1,14 +1,27 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Utils from './Utils.js';
+import Config from './Config.js';
 
-const initalState = Array(9).fill(null);
+const handleFieldClick = (state, action) => {
+    const changePlayer = (currentPlayer, setPlayer) => {
+        if (currentPlayer === Config.playerNames.playerOne) {
+            setPlayer(Config.playerNames.playerTwo);
+        } else if (currentPlayer === Config.playerNames.playerTwo) {
+            setPlayer(Config.playerNames.playerOne);
+        } else {
+            console.log('Unrecognized player');
+        }
+    };
 
-const reducer = (state, action) => {
     let newState = [...state];
     console.log('Clicked', action.fieldIndex);
     if (state[action.fieldIndex] == null) {
         console.log('New mark');
-        newState[action.fieldIndex] = 'cross';
+        newState[action.fieldIndex] = action.currentPlayer;
+
+        changePlayer(action.currentPlayer, action.setPlayer);
+        action.incRoundNumber();
     } else {
         console.log('Already checked');
     }
@@ -16,27 +29,78 @@ const reducer = (state, action) => {
     return newState;
 };
 
-const Table = () => {
-    const [fields, setFields] = useReducer(reducer, initalState);
+const calculateWinner = action => {
+    const winningLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    if (!action.board.includes(null)) {
+        console.log('Game draw');
+        action.setGameState(Config.gameStates.draw);
+        return 'none';
+    }
+
+    winningLines.forEach(line => {
+        const [a, b, c] = line;
+        if (
+            action.board[a] &&
+            action.board[a] === action.board[b] &&
+            action.board[b] === action.board[c]
+        ) {
+            console.log('Game won by: ' + action.board[a]);
+            action.setGameState(Config.gameStates.won);
+            action.setWinner(action.board[a]);
+            return action.board[a];
+        }
+    });
+
+    return null;
+};
+
+const Table = props => {
+    const initalState = Array(9).fill(null);
+    const [fields, setFields] = useReducer(handleFieldClick, initalState);
+
+    useEffect(() => {
+        calculateWinner({
+            board: fields,
+            setGameState: props.setGameState,
+            setWinner: props.setWinner
+        });
+    }, [fields]);
 
     const renderButton = (id, state) => {
-        let content = '';
-        if (state === 'cross') {
-            content = 'X';
-        } else if (state === 'circle') {
-            content = 'O';
-        }
+        const getContentForState = state => {
+            return state == null ? '_' : state;
+        };
+
+        let content = getContentForState(state);
 
         return (
-            <button
-                className="tic"
+            <div
+                className="table-cell"
                 key={id}
                 onClick={() => {
-                    setFields({ fieldIndex: id });
+                    props.gameState === Config.gameStates.ongoing
+                        ? setFields({
+                              fieldIndex: id,
+                              currentPlayer: props.playerName,
+                              setPlayer: props.setPlayerName,
+                              incRoundNumber: props.incRoundNumber,
+                              gameState: props.gameState
+                          })
+                        : console.log('Game is ended');
                 }}
             >
                 {content}
-            </button>
+            </div>
         );
     };
 
@@ -44,29 +108,26 @@ const Table = () => {
         <div className="table">
             <div className="table-body">
                 <div className="table-row">
-                    {Utils.range(0, 2).map(id => (
-                        <div className="table-cell" key={id}>
-                            {renderButton(id, fields[id])}
-                        </div>
-                    ))}
+                    {Utils.range(0, 2).map(id => renderButton(id, fields[id]))}
                 </div>
                 <div className="table-row">
-                    {Utils.range(3, 5).map(id => (
-                        <div className="table-cell" key={id}>
-                            {renderButton(id, fields[id])}
-                        </div>
-                    ))}
+                    {Utils.range(3, 5).map(id => renderButton(id, fields[id]))}
                 </div>
                 <div className="table-row">
-                    {Utils.range(6, 8).map(id => (
-                        <div className="table-cell" key={id}>
-                            {renderButton(id, fields[id])}
-                        </div>
-                    ))}
+                    {Utils.range(6, 8).map(id => renderButton(id, fields[id]))}
                 </div>
             </div>
         </div>
     );
+};
+
+Table.propTypes = {
+    playerName: PropTypes.string.isRequired,
+    setPlayerName: PropTypes.func.isRequired,
+    incRoundNumber: PropTypes.func.isRequired,
+    gameState: PropTypes.string.isRequired,
+    setGameState: PropTypes.func.isRequired,
+    setWinner: PropTypes.func.isRequired
 };
 
 export default Table;
