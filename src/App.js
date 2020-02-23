656dc62d6d103de415cf62a87a99dce6ebbe70a1
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2020-02-23 22:13:09 +0100

    App updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from './Table.js';
+import Title from './Title.js';
+import Config from './Config.js';
 import './styles.sass';
 
 const App = () => {
-  return (
-    <div className="game">
-      <Table />
-    </div>
-  );
+    const [player, setPlayer] = useState(Config.playerNames.playerOne);
+    const [roundNumber, setRoundNumber] = useState(1);
+    const [winner, setWinner] = useState(null);
+    const [gameState, setGameState] = useState(Config.gameStates.ongoing);
+
+    return (
+        <>
+            <div className="app">
+                <Title
+                    playerName={player}
+                    roundNumber={roundNumber}
+                    gameState={gameState}
+                    winnerName={winner}
+                />
+                <div className="game">
+                    <Table
+                        playerName={player}
+                        setPlayerName={setPlayer}
+                        incRoundNumber={() => setRoundNumber(roundNumber + 1)}
+                        gameState={gameState}
+                        setGameState={setGameState}
+                        setWinner={setWinner}
+                    />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default App;
