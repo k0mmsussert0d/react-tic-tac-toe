bb080587e3868fdc7b8f4c33126097c3e74fcac8
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2020-02-23 22:35:49 +0100

    Updates

---
+++ precache-manifest.d25649c622d3423d54f438a8fdabb51c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f4847cafc34b0fb4bb250851eab937",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "297c5635c39a8d226987",
+    "url": "/tic-tac-toe/static/css/main.ee35b341.chunk.css"
+  },
+  {
+    "revision": "49a750e1fa0d7c3d3114",
+    "url": "/tic-tac-toe/static/js/2.aef1a5b9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe/static/js/2.aef1a5b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297c5635c39a8d226987",
+    "url": "/tic-tac-toe/static/js/main.62476a84.chunk.js"
+  },
+  {
+    "revision": "e0ef8349fddbcbc67fbb",
+    "url": "/tic-tac-toe/static/js/runtime-main.c9c4acdf.js"
+  }
+]);
